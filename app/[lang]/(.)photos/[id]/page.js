2693ce784cd0889610547647a4d43bbaa9d80875
Pedp4 WPBX4125BLF102401423 <sihabarnab123@gmail.com>
2025-07-30 13:48:18 +0600

    create intercepting routing and update internationalization

---
+++ app/[lang]/(.)photos/[id]/page.js
@@ -0,0 +1,11 @@
+import Modal from "@/components/Modal";
+import PhotoDetails from "@/components/PhotoDetails";
+
+const PhotoModal = ({ params: { id, lang } }) => {
+  return (
+    <Modal>
+      <PhotoDetails id={id} lang={lang} />
+    </Modal>
+  );
+};
+export default PhotoModal;
